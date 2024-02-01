6441a6156722775d1391ec6144fe6d8aaec227d4
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-02-01 14:18:05 +0545

    changes|

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -4,7 +4,7 @@ function HomePage() {
     return (
         <div style={{
             backgroundImage: "url('/images/EarthBg.svg')",
-            backgroundRepeat: "no-repeat",
+            backgroundRepeat: "no-repeat", 
             backgroundSize: "cover",
 
         }} className={"text-8xl font-Bellefair w-screen h-screen"} >So you want to space travel?</div>
